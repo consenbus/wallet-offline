f49684ef533ac6c803ada723b6bdef5da6dc02b8
Redstone Zhao <13740080@qq.com>
2018-03-21 15:38:47 +0800

    Add pow localStore autoBackup/autoRestore

--- src/utils/wallet/pow.js
+++ src/utils/wallet/pow.js
@@ -8,8 +8,42 @@
  */
 
 /* eslint-disable no-undef */
+import store from '../store';
 
 const dict = {};
+
+const storeKey = 'wallet-works';
+
+const backup2Storage = () => {
+  const works = [];
+  Object.keys(dict).forEach((key) => {
+    const item = dict[key];
+    if (item.state !== 'done') return;
+    works.push([key, item.work, item.completed]);
+  });
+  store.setItem(storeKey, works);
+};
+
+// micro seconds of one week
+const expired = 7 * 86400 * 1000;
+const restore = () => {
+  const now = Date.now();
+  store.getItem(storeKey).then((works) => {
+    works.forEach(([hash, work, completed]) => {
+      // ignore too old item
+      if (now - completed > expired) return;
+      dict[hash] = {
+        state: 'done',
+        work,
+        completed,
+        spent_MS: 0,
+        promise: new Promise((resolve) => {
+          resolve(work);
+        }),
+      };
+    });
+  });
+};
 
 const NUM_THREADS = 3;
 
@@ -25,10 +59,14 @@ const createTask = (hash) => {
     }, (data) => {
       work.state = 'done';
       work.work = data;
+      work.completed = Date.now();
       work.spent_MS = Date.now() - start;
 
       console.log('Pow calc hash is: %s, work is: %s, spent_MS: %d, started: %d, completed: %d', hash, data, work.spent_MS, start, Date.now());
       resolve(data);
+
+      // write into localStorage for quick to get
+      setTimeout(backup2Storage, 10);
     });
   });
   work.promise = promise;
@@ -48,6 +86,9 @@ const get = async (hash) => {
 
 window.__powCalcPool = dict;
 window.__pow = get;
+
+// Auto restore from localStorage
+restore();
 
 export default get;
 /* eslint-enable no-undef */

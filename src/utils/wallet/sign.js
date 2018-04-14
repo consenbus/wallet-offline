adfcad36fbd8a4205e3894f1ad4fac895b3d968e
Redstone Zhao <13740080@qq.com>
2018-04-14 14:54:54 +0800

    optimization signature

--- src/utils/wallet/sign.js
+++ src/utils/wallet/sign.js
@@ -2,67 +2,29 @@ const _functions = require('./functions');
 const nacl = require('./nacl'); // We are using a forked version of tweetnacl, so need to import nacl
 const blake = require('blakejs');
 
-function sendSign({ previous, destination, balance }, privateKey) {
-  const context = blake.blake2bInit(32, null);
-  blake.blake2bUpdate(context, (0, _functions.hex_uint8)(previous));
-  blake.blake2bUpdate(context, (0, _functions.hex_uint8)(destination));
-  blake.blake2bUpdate(context, (0, _functions.hex_uint8)(balance));
-  const hash = (0, _functions.uint8_hex)(blake.blake2bFinal(context));
-
-  const xhash = (0, _functions.hex_uint8)(hash);
-  const xkey = (0, _functions.hex_uint8)(privateKey);
-  // console.log(hash, xhash);
-  const xsign = nacl.sign.detached(xhash, xkey);
-  return (0, _functions.uint8_hex)(xsign);
-}
-
-function openSign({ account, representative, source }, privateKey) {
-  const context = blake.blake2bInit(32, null);
-  blake.blake2bUpdate(context, (0, _functions.hex_uint8)(source));
-  blake.blake2bUpdate(context, (0, _functions.hex_uint8)(representative));
-  blake.blake2bUpdate(context, (0, _functions.hex_uint8)(account));
-  const hash = (0, _functions.uint8_hex)(blake.blake2bFinal(context));
-
-  const xhash = (0, _functions.hex_uint8)(hash);
-  const xkey = (0, _functions.hex_uint8)(privateKey);
-  // console.log(hash, xhash);
-  const xsign = nacl.sign.detached(xhash, xkey);
-  return (0, _functions.uint8_hex)(xsign);
-}
-
-function receiveSign({ previous, source }, privateKey) {
-  const context = blake.blake2bInit(32, null);
-  blake.blake2bUpdate(context, (0, _functions.hex_uint8)(previous));
-  blake.blake2bUpdate(context, (0, _functions.hex_uint8)(source));
-  const hash = (0, _functions.uint8_hex)(blake.blake2bFinal(context));
-
-  const xhash = (0, _functions.hex_uint8)(hash);
-  const xkey = (0, _functions.hex_uint8)(privateKey);
-  // console.log(hash, xhash);
-  const xsign = nacl.sign.detached(xhash, xkey);
-  return (0, _functions.uint8_hex)(xsign);
-}
-
-function changeSign({ previous, destination, balance }, privateKey) {
-  const context = blake.blake2bInit(32, null);
-  blake.blake2bUpdate(context, (0, _functions.hex_uint8)(previous));
-  blake.blake2bUpdate(context, (0, _functions.hex_uint8)(destination));
-  blake.blake2bUpdate(context, (0, _functions.hex_uint8)(balance));
-  const hash = (0, _functions.uint8_hex)(blake.blake2bFinal(context));
-
-  const xhash = (0, _functions.hex_uint8)(hash);
-  const xkey = (0, _functions.hex_uint8)(privateKey);
-  // console.log(hash, xhash);
-  const xsign = nacl.sign.detached(xhash, xkey);
-  return (0, _functions.uint8_hex)(xsign);
-}
+const fieldDict = {
+  send: ['previous', 'destination', 'balance'],
+  open: ['source', 'representative', 'account'],
+  receive: ['previous', 'source'],
+  change: ['previous', 'destination', 'balance'],
+};
 
 function sign(params, privateKey) {
-  if (params.type === 'send') return sendSign(params, privateKey);
-  if (params.type === 'receive') return receiveSign(params, privateKey);
-  if (params.type === 'open') return openSign(params, privateKey);
-  if (params.type === 'change') return changeSign(params, privateKey);
-  throw new Error('Unkonw block type');
+  const fields = fieldDict[params.type];
+  if (!fields) throw new Error('Unkonw block type');
+
+  const context = blake.blake2bInit(32, null);
+  fields.forEach((field) => {
+    blake.blake2bUpdate(context, (0, _functions.hex_uint8)(params[field]));
+  });
+
+  const hash = (0, _functions.uint8_hex)(blake.blake2bFinal(context));
+
+  const xhash = (0, _functions.hex_uint8)(hash);
+  const xkey = (0, _functions.hex_uint8)(privateKey);
+  // console.log(hash, xhash);
+  const xsign = nacl.sign.detached(xhash, xkey);
+  return (0, _functions.uint8_hex)(xsign);
 }
 
 module.exports = sign;

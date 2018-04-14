ed50f861640091ffe05ca4533d1f6589837a271a
Redstone Zhao <13740080@qq.com>
2018-04-14 11:31:09 +0800

    eslintify

--- src/utils/wallet/functions.js
+++ src/utils/wallet/functions.js
@@ -1,35 +1,32 @@
-import blake from "blakejs";
+import blake from 'blakejs';
 
-export const stringFromHex = function(ihex) {
-  var hex = ihex.toString(); //force conversion
-  var str = "";
-  for (var i = 0; i < hex.length; i += 2)
-    str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+export const stringFromHex = function (ihex) {
+  const hex = ihex.toString(); // force conversion
+  let str = '';
+  for (let i = 0; i < hex.length; i += 2) { str += String.fromCharCode(parseInt(hex.substr(i, 2), 16)); }
   return str;
 };
 
-export const stringToHex = function(str) {
-  var hex = "";
-  for (var i = 0; i < str.length; i++) {
-    hex += "" + str.charCodeAt(i).toString(16);
+export const stringToHex = function (str) {
+  let hex = '';
+  for (let i = 0; i < str.length; i++) {
+    hex += `${str.charCodeAt(i).toString(16)}`;
   }
   return hex;
 };
 
-export const accountFromHexKey = function(hex) {
-  var checksum = "";
-  var key_bytes = uint4_uint8(hex_uint4(hex));
-  checksum = uint5_string(
-    uint4_uint5(uint8_uint4(blake.blake2b(key_bytes, null, 5).reverse()))
-  );
-  var c_account = uint5_string(uint4_uint5(hex_uint4("0" + hex)));
-  return "xrb_" + c_account + checksum;
+export const accountFromHexKey = function (hex) {
+  let checksum = '';
+  const key_bytes = uint4_uint8(hex_uint4(hex));
+  checksum = uint5_string(uint4_uint5(uint8_uint4(blake.blake2b(key_bytes, null, 5).reverse())));
+  const c_account = uint5_string(uint4_uint5(hex_uint4(`0${hex}`)));
+  return `bus_${c_account}${checksum}`;
 };
 
-export const parseXRBAccount = function(str) {
-  var i = str.indexOf("xrb_");
+export const parseXRBAccount = function (str) {
+  const i = str.indexOf('xrb_');
   if (i !== -1) {
-    var acc = str.slice(i, i + 64);
+    const acc = str.slice(i, i + 64);
     try {
       keyFromAccount(acc);
       return acc;
@@ -40,8 +37,8 @@ export const parseXRBAccount = function(str) {
   return false;
 };
 
-export const dec2hex = function(str, bytes = null) {
-  var dec = str.toString().split(""),
+export const dec2hex = function (str, bytes = null) {
+  let dec = str.toString().split(''),
     sum = [],
     hex = [],
     i,
@@ -58,39 +55,39 @@ export const dec2hex = function(str, bytes = null) {
     hex.push(sum.pop().toString(16));
   }
 
-  hex = hex.join("");
+  hex = hex.join('');
 
-  if (hex.length % 2 !== 0) hex = "0" + hex;
+  if (hex.length % 2 !== 0) hex = `0${hex}`;
 
   if (bytes > hex.length / 2) {
-    var diff = bytes - hex.length / 2;
-    for (var j = 0; j < diff; j++) hex = "00" + hex;
+    const diff = bytes - hex.length / 2;
+    for (let j = 0; j < diff; j++) hex = `00${hex}`;
   }
 
   return hex;
 };
 
-export const hex2dec = function(s) {
+export const hex2dec = function (s) {
   function add(ix, iy) {
-    var c = 0,
+    let c = 0,
       r = [];
-    var x = ix.split("").map(Number);
-    var y = iy.split("").map(Number);
+    const x = ix.split('').map(Number);
+    const y = iy.split('').map(Number);
     while (x.length || y.length) {
-      var s = (x.pop() || 0) + (y.pop() || 0) + c;
+      const s = (x.pop() || 0) + (y.pop() || 0) + c;
       r.unshift(s < 10 ? s : s - 10);
       c = s < 10 ? 0 : 1;
     }
     if (c) r.unshift(c);
-    return r.join("");
+    return r.join('');
   }
 
-  var dec = "0";
-  s.split("").forEach(function(chr) {
-    var n = parseInt(chr, 16);
-    for (var t = 8; t; t >>= 1) {
+  let dec = '0';
+  s.split('').forEach((chr) => {
+    const n = parseInt(chr, 16);
+    for (let t = 8; t; t >>= 1) {
       dec = add(dec, dec);
-      if (n & t) dec = add(dec, "1");
+      if (n & t) dec = add(dec, '1');
     }
   });
   return dec;
@@ -124,8 +121,8 @@ export const hex2dec = function(s) {
 
 // Arrays manipulations
 function uint8_uint4(uint8) {
-  var length = uint8.length;
-  var uint4 = new Uint8Array(length * 2);
+  const length = uint8.length;
+  const uint4 = new Uint8Array(length * 2);
   for (let i = 0; i < length; i++) {
     uint4[i * 2] = (uint8[i] / 16) | 0;
     uint4[i * 2 + 1] = uint8[i] % 16;
@@ -134,21 +131,20 @@ function uint8_uint4(uint8) {
 }
 
 function uint4_uint8(uint4) {
-  var length = uint4.length / 2;
-  var uint8 = new Uint8Array(length);
-  for (let i = 0; i < length; i++)
-    uint8[i] = uint4[i * 2] * 16 + uint4[i * 2 + 1];
+  const length = uint4.length / 2;
+  const uint8 = new Uint8Array(length);
+  for (let i = 0; i < length; i++) { uint8[i] = uint4[i * 2] * 16 + uint4[i * 2 + 1]; }
   return uint8;
 }
 
 function uint4_uint5(uint4) {
-  var length = uint4.length / 5 * 4;
-  var uint5 = new Uint8Array(length);
+  const length = uint4.length / 5 * 4;
+  const uint5 = new Uint8Array(length);
   for (let i = 1; i <= length; i++) {
-    let n = i - 1;
-    let m = i % 4;
-    let z = n + (i - m) / 4;
-    let right = uint4[z] << m;
+    const n = i - 1;
+    const m = i % 4;
+    const z = n + (i - m) / 4;
+    const right = uint4[z] << m;
     let left;
     if ((length - i) % 4 === 0) left = uint4[z - 1] << 4;
     else left = uint4[z + 1] >> (4 - m);
@@ -158,67 +154,64 @@ function uint4_uint5(uint4) {
 }
 
 function uint5_uint4(uint5) {
-  var length = uint5.length / 4 * 5;
-  var uint4 = new Uint8Array(length);
+  const length = uint5.length / 4 * 5;
+  const uint4 = new Uint8Array(length);
   for (let i = 1; i <= length; i++) {
-    let n = i - 1;
-    let m = i % 5;
-    let z = n - (i - m) / 5;
-    let right = uint5[z - 1] << (5 - m);
-    let left = uint5[z] >> m;
+    const n = i - 1;
+    const m = i % 5;
+    const z = n - (i - m) / 5;
+    const right = uint5[z - 1] << (5 - m);
+    const left = uint5[z] >> m;
     uint4[n] = (left + right) % 16;
   }
   return uint4;
 }
 
 function string_uint5(string) {
-  var letter_list = "13456789abcdefghijkmnopqrstuwxyz".split("");
-  var length = string.length;
-  var string_array = string.split("");
-  var uint5 = new Uint8Array(length);
-  for (let i = 0; i < length; i++)
-    uint5[i] = letter_list.indexOf(string_array[i]);
+  const letter_list = '13456789abcdefghijkmnopqrstuwxyz'.split('');
+  const length = string.length;
+  const string_array = string.split('');
+  const uint5 = new Uint8Array(length);
+  for (let i = 0; i < length; i++) { uint5[i] = letter_list.indexOf(string_array[i]); }
   return uint5;
 }
 
 function uint5_string(uint5) {
-  var letter_list = "13456789abcdefghijkmnopqrstuwxyz".split("");
-  var string = "";
+  const letter_list = '13456789abcdefghijkmnopqrstuwxyz'.split('');
+  let string = '';
   for (let i = 0; i < uint5.length; i++) string += letter_list[uint5[i]];
   return string;
 }
 
-export const hex_uint8 = function(hex) {
-  var length = (hex.length / 2) | 0;
-  var uint8 = new Uint8Array(length);
-  for (let i = 0; i < length; i++)
-    uint8[i] = parseInt(hex.substr(i * 2, 2), 16);
+export const hex_uint8 = function (hex) {
+  const length = (hex.length / 2) | 0;
+  const uint8 = new Uint8Array(length);
+  for (let i = 0; i < length; i++) { uint8[i] = parseInt(hex.substr(i * 2, 2), 16); }
   return uint8;
 };
 
 function hex_uint4(hex) {
-  var length = hex.length;
-  var uint4 = new Uint8Array(length);
+  const length = hex.length;
+  const uint4 = new Uint8Array(length);
   for (let i = 0; i < length; i++) uint4[i] = parseInt(hex.substr(i, 1), 16);
   return uint4;
 }
 
-export const uint8_hex = function(uint8) {
-  var hex = "";
+export const uint8_hex = function (uint8) {
+  let hex = '';
   let aux;
   for (let i = 0; i < uint8.length; i++) {
     aux = uint8[i].toString(16).toUpperCase();
-    if (aux.length === 1) aux = "0" + aux;
+    if (aux.length === 1) aux = `0${aux}`;
     hex += aux;
-    aux = "";
+    aux = '';
   }
   return hex;
 };
 
-export const uint4_hex = function(uint4) {
-  var hex = "";
-  for (let i = 0; i < uint4.length; i++)
-    hex += uint4[i].toString(16).toUpperCase();
+export const uint4_hex = function (uint4) {
+  let hex = '';
+  for (let i = 0; i < uint4.length; i++) { hex += uint4[i].toString(16).toUpperCase(); }
   return hex;
 };
 
@@ -230,33 +223,29 @@ function equal_arrays(array1, array2) {
 }
 
 function array_crop(array) {
-  var length = array.length - 1;
-  var cropped_array = new Uint8Array(length);
+  const length = array.length - 1;
+  const cropped_array = new Uint8Array(length);
   for (let i = 0; i < length; i++) cropped_array[i] = array[i + 1];
   return cropped_array;
 }
 
-export const keyFromAccount = function(account) {
+export const keyFromAccount = function (account) {
   if (
-    (account.startsWith("xrb_1") || account.startsWith("xrb_3")) &&
+    (account.startsWith('xrb_1') || account.startsWith('xrb_3')) &&
     account.length === 64
   ) {
-    var account_crop = account.substring(4, 64);
-    var isValid = /^[13456789abcdefghijkmnopqrstuwxyz]+$/.test(account_crop);
+    const account_crop = account.substring(4, 64);
+    const isValid = /^[13456789abcdefghijkmnopqrstuwxyz]+$/.test(account_crop);
     if (isValid) {
-      var key_uint4 = array_crop(
-        uint5_uint4(string_uint5(account_crop.substring(0, 52)))
-      );
-      var hash_uint4 = uint5_uint4(
-        string_uint5(account_crop.substring(52, 60))
-      );
-      var key_array = uint4_uint8(key_uint4);
-      var blake_hash = blake.blake2b(key_array, null, 5).reverse();
+      const key_uint4 = array_crop(uint5_uint4(string_uint5(account_crop.substring(0, 52))));
+      const hash_uint4 = uint5_uint4(string_uint5(account_crop.substring(52, 60)));
+      const key_array = uint4_uint8(key_uint4);
+      const blake_hash = blake.blake2b(key_array, null, 5).reverse();
       if (equal_arrays(hash_uint4, uint8_uint4(blake_hash))) {
-        var key = uint4_hex(key_uint4);
+        const key = uint4_hex(key_uint4);
         return key;
-      } else throw new Error("Checksum incorrect.");
-    } else throw new Error("Invalid XRB account.");
+      } throw new Error('Checksum incorrect.');
+    } else throw new Error('Invalid XRB account.');
   }
-  throw new Error("Invalid XRB account.");
+  throw new Error('Invalid XRB account.');
 };

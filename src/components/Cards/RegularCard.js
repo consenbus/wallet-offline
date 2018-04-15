a45c83359d8f414587068b3f672ecb1424484d93
Redstone Zhao <13740080@qq.com>
2018-04-15 14:38:01 +0800

    remove useless module file

--- src/components/Cards/RegularCard.js
+++ src/components/Cards/RegularCard.js
@@ -1,15 +1,15 @@
-import React from "react";
+import React from 'react';
 import {
   withStyles,
   Card,
   CardContent,
   CardHeader,
-  CardActions
-} from "material-ui";
-import PropTypes from "prop-types";
-import cx from "classnames";
+  CardActions,
+} from 'material-ui';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
 
-import regularCardStyle from "../../variables/styles/regularCardStyle";
+import regularCardStyle from '../../variables/styles/regularCardStyle';
 
 function RegularCard({ ...props }) {
   const {
@@ -20,25 +20,25 @@ function RegularCard({ ...props }) {
     cardSubtitle,
     content,
     plainContent,
-    footer
+    footer,
   } = props;
   const plainCardClasses = cx({
-    [" " + classes.cardPlain]: plainCard
+    [` ${classes.cardPlain}`]: plainCard,
   });
   const cardPlainHeaderClasses = cx({
-    [" " + classes.cardPlainHeader]: plainCard
+    [` ${classes.cardPlainHeader}`]: plainCard,
   });
   return (
     <Card className={classes.card + plainCardClasses}>
       <CardHeader
         classes={{
-          root:
-            classes.cardHeader +
-            " " +
-            classes[headerColor + "CardHeader"] +
+          root: [
+            classes.cardHeader,
+            classes[`${headerColor}CardHeader`],
             cardPlainHeaderClasses,
+          ].join(' '),
           title: classes.cardTitle,
-          subheader: classes.cardSubtitle
+          subheader: classes.cardSubtitle,
         }}
         title={cardTitle}
         subheader={cardSubtitle}
@@ -58,25 +58,25 @@ function RegularCard({ ...props }) {
 }
 
 RegularCard.defaultProps = {
-  headerColor: "grey"
+  headerColor: 'grey',
 };
 
 RegularCard.propTypes = {
   plainCard: PropTypes.bool,
   classes: PropTypes.object.isRequired,
   headerColor: PropTypes.oneOf([
-    "orange",
-    "green",
-    "red",
-    "blue",
-    "purple",
-    "grey"
+    'orange',
+    'green',
+    'red',
+    'blue',
+    'purple',
+    'grey',
   ]),
   cardTitle: PropTypes.node,
   cardSubtitle: PropTypes.node,
   content: PropTypes.node,
   plainContent: PropTypes.bool,
-  footer: PropTypes.node
+  footer: PropTypes.node,
 };
 
 export default withStyles(regularCardStyle)(RegularCard);

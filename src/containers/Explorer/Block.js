9d20cfb1794fe567b6fa5d233f70d83619a7d695
Redstone Zhao <13740080@qq.com>
2018-05-14 23:24:57 +0800

    Block remove inject account, trun off one eslint.rule

--- src/containers/Explorer/Block.js
+++ src/containers/Explorer/Block.js
@@ -1,67 +1,82 @@
-import React from 'react';
-import _isEmpty from 'lodash/isEmpty';
-import Grid from 'material-ui/Grid';
-import { observer, inject } from 'mobx-react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import _isEmpty from "lodash/isEmpty";
+import Grid from "material-ui/Grid";
+import { Link } from "react-router-dom";
+import { withStyles } from "material-ui/styles";
+import { LinearProgress } from "material-ui/Progress";
+import ConsenbusWalletCore from "consenbus-wallet-core";
 
-import { withStyles } from 'material-ui/styles';
-import pink from 'material-ui/colors/pink';
-import green from 'material-ui/colors/green';
+import RegularCard from "../../components/Cards/RegularCard";
+import ItemGrid from "../../components/Grid/ItemGrid";
+import Table from "../../components/Table/Table";
+import converter from "../../utils/converter";
 
-import { LinearProgress } from 'material-ui/Progress';
+import Layout from "../Home/_Layout";
+import Header from "../Home/Tab/_Header";
+import rpc from "../../utils/rpc";
 
-import RegularCard from '../../components/Cards/RegularCard';
-import ItemGrid from '../../components/Grid/ItemGrid';
-import Table from '../../components/Table/Table';
-import converter from '../../utils/converter';
-import { hex2dec } from '../../utils/wallet/functions';
-
-import Layout from '../Home/_Layout';
-import Header from '../Home/Tab/_Header';
-import rpc from '../../utils/rpc';
+const {
+  fns: { hex2dec }
+} = ConsenbusWalletCore;
 
 const styles = {
-  avatar: {},
-  pinkAvatar: {
-    color: '#fff',
-    backgroundColor: pink[500],
-    marginRight: '10px',
-  },
-  greenAvatar: {
-    color: '#fff',
-    backgroundColor: green[500],
-    marginRight: '10px',
-  },
   error: {
-    border: '1px solid #ffa39e',
-    backgroundColor: '#fff1f0',
-    padding: '5px',
-    margin: '10px',
-  },
+    border: "1px solid #ffa39e",
+    backgroundColor: "#fff1f0",
+    padding: "5px",
+    margin: "10px"
+  }
 };
 
-const getTableData = (block) => {
+const getTableData = block => {
   if (_isEmpty(block)) {
     return [];
   }
 
   const {
-    hash, type, account, balance, destination, amount, source, previous, work, signature,
+    hash,
+    type,
+    account,
+    balance,
+    destination,
+    amount,
+    source,
+    previous,
+    work,
+    signature
   } = block;
 
-  const data = [
-    ['Hash', <code>{hash}</code>],
-    ['Type', <code>{type}</code>],
-  ];
+  const data = [["Hash", <code>{hash}</code>], ["Type", <code>{type}</code>]];
 
-  if (account) data.push(['Account', <Link to={`/explorer/accounts/${account}`}>{account}</Link>]);
-  if (balance) data.push(['Balance', `${converter.unit(hex2dec(balance || 0), 'raw', 'BUS')} BUS`]);
-  if (destination) data.push(['Destination', <Link to={`/explorer/accounts/${destination}`}>{destination}</Link>]);
-  if (amount) data.push(['Amount', `${converter.unit(amount || 0, 'raw', 'BUS')} BUS`]);
-  if (source) data.push(['Source', <Link to={`/explorer/blocks/${source}`}>{source}</Link>]);
-  if (previous) data.push(['Previous', <Link to={`/explorer/blocks/${previous}`}>{previous}</Link>]);
-  data.push(['Work', <code>{work}</code>]);
-  data.push(['Signature', <code>{signature}</code>]);
+  if (account)
+    data.push([
+      "Account",
+      <Link to={`/explorer/accounts/${account}`}>{account}</Link>
+    ]);
+  if (balance)
+    data.push([
+      "Balance",
+      `${converter.unit(hex2dec(balance || 0), "raw", "BUS")} BUS`
+    ]);
+  if (destination)
+    data.push([
+      "Destination",
+      <Link to={`/explorer/accounts/${destination}`}>{destination}</Link>
+    ]);
+  if (amount)
+    data.push(["Amount", `${converter.unit(amount || 0, "raw", "BUS")} BUS`]);
+  if (source)
+    data.push([
+      "Source",
+      <Link to={`/explorer/blocks/${source}`}>{source}</Link>
+    ]);
+  if (previous)
+    data.push([
+      "Previous",
+      <Link to={`/explorer/blocks/${previous}`}>{previous}</Link>
+    ]);
+  data.push(["Work", <code>{work}</code>]);
+  data.push(["Signature", <code>{signature}</code>]);
 
   return data;
 };
@@ -70,8 +85,8 @@ class Show extends React.Component {
   state = {
     block: null, // current select block detail from rpc
     loading: false, // get block detail from rpc process
-    error: null, // error message at get block detail from rpc process
-  }
+    error: null // error message at get block detail from rpc process
+  };
 
   componentWillMount() {
     const currentHash = this.props.match.params.hash;
@@ -88,27 +103,30 @@ class Show extends React.Component {
     this.setState({
       block: null,
       loading: true,
-      error: null,
+      error: null
     });
     try {
-      const { data } = await rpc.post('/', {
-        action: 'blocks_info',
-        hashes: [hash],
+      const { data } = await rpc.post("/", {
+        action: "blocks_info",
+        hashes: [hash]
       });
       const info = data.blocks[hash];
       this.setState({
-        block: Object.assign({
-          hash,
-          account: info.block_account,
-          amount: info.amount,
-        }, JSON.parse(info.contents)),
+        block: Object.assign(
+          {
+            hash,
+            account: info.block_account,
+            amount: info.amount
+          },
+          JSON.parse(info.contents)
+        ),
         error: null,
-        loading: false,
+        loading: false
       });
     } catch (error) {
       this.setState({
         loading: false,
-        error,
+        error
       });
     }
   }
@@ -124,19 +142,21 @@ class Show extends React.Component {
         <Grid container>
           {loading && <LinearProgress />}
           {error && <div className={classes.error}>{error.message}</div>}
-          {!loading && !error && block && (
-            <ItemGrid xs={12} sm={12} md={12}>
-              <RegularCard
-                cardTitle="Block"
-                plainContent
-                content={<Table tableData={tableData} />}
-              />
-            </ItemGrid>
-          )}
+          {!loading &&
+            !error &&
+            block && (
+              <ItemGrid xs={12} sm={12} md={12}>
+                <RegularCard
+                  cardTitle="Block"
+                  plainContent
+                  content={<Table tableData={tableData} />}
+                />
+              </ItemGrid>
+            )}
         </Grid>
       </Layout>
     );
   }
 }
 
-export default withStyles(styles)(inject('account')(observer(Show)));
+export default withStyles(styles)(Show);

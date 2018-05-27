c7f07a7599e2a51751db5726c89a410fcc262ae7
Redstone Zhao <13740080@qq.com>
2018-05-27 09:11:45 +0800

    restore rpc address

--- src/utils/rpc.js
+++ src/utils/rpc.js
@@ -2,10 +2,7 @@ import axios from "axios";
 import retry from "async/retry";
 
 let enabledBaseURLIndex = 0;
-const baseURLs = [
-  "http://127.0.0.1:55000"
-  // 'http://node-test.consenbus.org:55000',
-];
+const baseURLs = ["http://node-test.consenbus.org:55000"];
 
 /* production
 const baseURLs = [

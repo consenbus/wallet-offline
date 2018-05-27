d42283b2a37b326e3b31d70b0c6c56bb1cd1c8ef
Redstone Zhao <13740080@qq.com>
2018-05-27 22:01:11 +0800

    Rmoeve props button on the first ListItem

--- src/containers/Home/Tab/Setting.js
+++ src/containers/Home/Tab/Setting.js
@@ -1,50 +1,50 @@
-import React, { Component } from 'react';
-import { observer, inject } from 'mobx-react';
-import { Redirect, Link } from 'react-router-dom';
-import { withStyles } from 'material-ui/styles';
-import List, { ListItem, ListItemText } from 'material-ui/List';
-import Divider from 'material-ui/Divider';
-import Card from 'material-ui/Card';
-import RightIcon from 'material-ui-icons/KeyboardArrowRight';
-import TextField from 'material-ui/TextField';
-import { MenuItem } from 'material-ui/Menu';
-import _map from 'lodash/map';
-import Layout from '../_Layout';
-import Header from './_Header';
-import Password from '../../../components/Password';
+import React, { Component } from "react";
+import { observer, inject } from "mobx-react";
+import { Redirect, Link } from "react-router-dom";
+import { withStyles } from "material-ui/styles";
+import List, { ListItem, ListItemText } from "material-ui/List";
+import Divider from "material-ui/Divider";
+import Card from "material-ui/Card";
+import RightIcon from "material-ui-icons/KeyboardArrowRight";
+import TextField from "material-ui/TextField";
+import { MenuItem } from "material-ui/Menu";
+import _map from "lodash/map";
+import Layout from "../_Layout";
+import Header from "./_Header";
+import Password from "../../../components/Password";
 
 const styles = theme => ({
   root: {
-    width: '100%',
+    width: "100%",
     maxWidth: 360,
-    backgroundColor: theme.palette.background.paper,
-  },
+    backgroundColor: theme.palette.background.paper
+  }
 });
 
 class Setting extends Component {
   state = {
     requestLogout: false,
-    logoutSuccess: false,
+    logoutSuccess: false
   };
 
   requestLogout = () => {
     this.setState({ requestLogout: true });
-  }
+  };
 
   clearTempData = () => {
     const { wallet } = this.props;
     wallet.clearTempData();
-  }
+  };
 
-  logout = (password) => {
+  logout = password => {
     const { wallet } = this.props;
     wallet.logout(password);
     if (!wallet.error) this.setState({ logoutSuccess: true });
-  }
+  };
 
-  handleChangeCurrent = (ev) => {
+  handleChangeCurrent = ev => {
     this.props.wallet.changeCurrent(ev.target.value);
-  }
+  };
 
   render() {
     const { requestLogout, logoutSuccess } = this.state;
@@ -65,14 +65,14 @@ class Setting extends Component {
           {logoutSuccess && (
             <Redirect
               to={{
-                pathname: '/guide',
-                state: { from: location },
+                pathname: "/guide",
+                state: { from: location }
               }}
             />
           )}
           <Card>
             <List component="nav">
-              <ListItem button>
+              <ListItem>
                 <TextField
                   id="select-account"
                   select
@@ -102,7 +102,10 @@ class Setting extends Component {
             <Divider />
             <List component="nav">
               <ListItem button onClick={this.requestLogout}>
-                <ListItemText primary="Security Preferences" secondary="Logout" />
+                <ListItemText
+                  primary="Security Preferences"
+                  secondary="Logout"
+                />
               </ListItem>
             </List>
           </Card>
@@ -112,4 +115,4 @@ class Setting extends Component {
   }
 }
 
-export default withStyles(styles)(inject('wallet')(observer(Setting)));
+export default withStyles(styles)(inject("wallet")(observer(Setting)));
